593e748c42da2b028f83aa1ac460e736dadd6406
dkd99 <dkd31994@gmail.com>
2024-06-16 17:08:38 +0530

    changes

---
+++ greatkart/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from store.models import Product
+def home(request):
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'home.html',context)
